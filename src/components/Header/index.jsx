93d607e725975b2a8bce5f30089aa3c592ed4d9a
Constantin <horghidanconstantin91@yahoo.com>
2024-03-20 18:28:01 +0000

    new code

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -19,7 +19,7 @@ function Header() {
 
         </ul>
 
-       <Link to="/my-saved-cars"><button>My Saved Cars</button></Link>
+       <Link to="/my-saved-cars"><button className='button'>My Saved Cars</button></Link>
     </div>
   );
 }
--- src/components/Logo/index.jsx
+++ src/components/Logo/index.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import "./style.css";
+import Logos from '../../assets/images/logo_1.png';
 
 function Logo() {
   return (
     <div className="">
-<img className="logoImage"src="src/assets/images/logo_1.png" alt="" />
+<img className="logoImage"src={Logos} alt="React Logo" />
 {/* <p className="wording">Connecting you with your car</p> */}
       
     </div>
