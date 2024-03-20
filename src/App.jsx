54fc0ec72f1b8d29f948c3037ee184e397470f7e
Constantin <horghidanconstantin91@yahoo.com>
2024-03-20 17:39:12 +0000

    pushed the updated project

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,39 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Footer from './components/Footer';
-import Wrapper from './components/Wrapper';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Footer from "./components/Footer";
+import Wrapper from "./components/Wrapper";
+import Home from "./pages/home/Home";
+import ProjectGallery from "./pages/ProjectGallery"; //- NOT USING THIS BUT IT#S STILL IN NAV BAR
+import Contact from "./pages/contact/Contact";
 
-import Header from './components/Header';
-import Home from './pages/Home';
-import ProjectGallery from './pages/ProjectGallery'; //- NOT USING THIS BUT IT#S STILL IN NAV BAR 
-import Contact from './pages/Contact';
-
-import carsData from './assets/car_data.json';
-import CarSearch from './components/CarSearch';
-
-
+import carsData from "./assets/car_data";
+import CarSearch from "./components/CarSearch";
+import ViewAll from "./pages/view-all/view-all";
+import AboutUs from "./pages/about us/about-us";
+import Header from "./components/Header";
+import MySavedCars from "./pages/my-saved-cars/my-saved-cars";
 
 function App() {
   return (
-
-    
-
     <Router>
-      <div>
+      <div className="sticky-top">  {/* This div is created and routered/ rendered from the Header components folder to display the Header here */}
         <Header />
-        <Wrapper>
-          <Routes>
-            {/* These placehopders can be swapped out Don't need portfolio / Projec*/}
-            <Route path="/" element={<Home/>} />
-            <Route path="/index" element={<Home/>} />
-            <Route path="/portfolio" element={<ProjectGallery/>} />
-            <Route path="/contact" element={<Contact/>} />
-          </Routes>
-          {/* Render CarSearch component here. But should probably be moved to Home page */}
-          <CarSearch data={carsData} type="Hatchback" location="London" />
-        </Wrapper>
-
-    
-        <Footer />
+      </div>
+      <div>
+      <Routes>
+        {/* These placehopders can be swapped out Don't need portfolio / Projec*/}
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/index" element={<Home />} /> */}
+        <Route path="/portfolio" element={<ProjectGallery />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/view-all" element={<ViewAll />} />
+        <Route path="/about-us" element={<AboutUs />} />
+        <Route path="/my-saved-cars" element={<MySavedCars />} />
+      </Routes>
+      <Footer />
       </div>
     </Router>
   );
 }
-
-
-
-
 
 export default App;
