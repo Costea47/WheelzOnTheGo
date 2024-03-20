7cd729e926af0c6472cf8222300aab462c6de472
Constantin <horghidanconstantin91@yahoo.com>
2024-03-20 20:58:41 +0000

    working code

--- src/components/Logo/index.jsx
+++ src/components/Logo/index.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import "./style.css";
-import Logos from '../../assets/images/logo_1.png';
 
 function Logo() {
   return (
     <div className="">
-<img className="logoImage"src={Logos} alt="React Logo" />
+<img className="logoImage"src="src/assets/images/logo_1.png" alt="" />
 {/* <p className="wording">Connecting you with your car</p> */}
       
     </div>
